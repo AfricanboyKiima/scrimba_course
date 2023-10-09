4452f19aee5d7ea0181cc2a81c2bfc69b273c044
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-09 04:47:34 +0100

    Added file to contains website js logic

---
+++ html/exercises/personal.js
@@ -0,0 +1,11 @@
+favouriteMovieGenre("cowboy")
+
+favouriteFruit("reqular")
+
+favouriteMode("banana")
+
+favouriteStyle("dark")
+
+function setProp(prop, value) {
+    document.documentElement.style.setProperty(prop, value)
+}
